e2b2d36d5804ddbb2a17f4b1db432ef315fe2ee9
lhb <782272323@qq.com>
2020-03-02 17:55:18 +0800

    测试部署环境

---
+++ My-Application-demo01/app/src/main/java/learn/lhb/my_application_demo01/MainActivity.java
@@ -0,0 +1,13 @@
+package learn.lhb.my_application_demo01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
